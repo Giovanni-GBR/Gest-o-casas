418aa8162624dd98f34203ff1773ed202e53b58b
Giovanni <giovanniheury@gmail.com>
2026-07-14 22:09:55 -0300

    fucão adicionar casa nova

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const modal = document.querySelector(".modal")
+const btnAddCasa = document.querySelector(".btn-add-casa")
+
 let totalCasas = 0
 let casasAlugadas = 0
 let casasLivres = 0
@@ -38,6 +41,10 @@ const casas = [
     }
 ]
 
+btnAddCasa.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
 function atualizarResumo() {
     const listaResumo = document.querySelector(".lista-resumo")
     let total = document.querySelector(".total")
@@ -52,10 +59,38 @@ function atualizarResumo() {
     atrasadas.textContent = casas.filter(casa => casa.status === "atrasado").length
 }
 
+function criarCasa() {
+    const nomeCasa = document.querySelector(".input-nome").value
+    const moradorCasa = document.querySelector(".input-morador").value
+    const valorCasa = document.querySelector(".input-valor").value
+    const statusCasa = document.querySelector("#statusCasa").value
+    const situacaoCasa = document.querySelector("#situacaoCasa").value
+
+    let casa = {
+        id: 5,
+        nome: nomeCasa,
+        morador: moradorCasa,
+        aluguel: valorCasa,
+        status: statusCasa,
+        situacao: situacaoCasa
+    }
+    
+    modal.style.display = "none"
+    
+    casas.push(casa)
+
+    atualizarResumo()
+    atualizarCasas()
+    
+}
+
 function atualizarCasas() {
     const listaCasas = document.querySelector(".lista-casas")
     
+    listaCasas.innerHTML = ""
+
     casas.forEach(casa => {
+        
         let li = document.createElement("li")
         li.classList.add("card-casa")
 
